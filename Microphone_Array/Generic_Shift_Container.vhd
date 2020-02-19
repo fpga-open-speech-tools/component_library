acc4abe04902dc6c018c29243919ac75a21aaef0
Tyler Davis <davis@flatearthinc.com>
2020-02-19 13:27:59 -0700

    Updated documentation

--- Microphone_Array/Generic_Shift_Container.vhd
+++ Microphone_Array/Generic_Shift_Container.vhd
@@ -1,5 +1,34 @@
-
-
+----------------------------------------------------------------------------
+--! @file Generic_Shift_Container.vhd
+--! @brief Generic shift register component
+--! @details  
+--! @author Tyler Davis
+--! @date 2020
+--! @copyright Copyright 2020 Flat Earth Inc
+--
+--  Permission is hereby granted, free of charge, to any person obtaining a copy
+--  of this software and associated documentation files (the "Software"), to deal
+--  IN the Software without restriction, including without limitation the rights
+--  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--  copies of the Software, and to permit persons to whom the Software is furnished
+--  to do so, subject to the following conditions:
+--
+--  The above copyright notice and this permission notice shall be included IN all
+--  copies or substantial portions of the Software.
+--
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+--  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+--  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+--  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+--  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+--  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--
+-- Tyler Davis
+-- Flat Earth Inc
+-- 985 Technology Blvd
+-- Bozeman, MT 59718
+-- support@flatearthinc.com
+----------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
@@ -21,7 +50,7 @@ architecture rtl of Generic_Shift_Container is
 signal output_data_r : std_logic_vector(data_width-1 downto 0) := (others => '0');
 
 begin 
-output_selet: process(clk)
+output_select: process(clk)
 begin
 
   if rising_edge(clk) then 
