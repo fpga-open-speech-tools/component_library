06594a50dbf237068e5d67c3497b49020ba23abd
Tyler Davis <davis@flatearthinc.com>
2020-02-19 13:07:55 -0700

    Updated documentation, removed debug signals, minor logic refactoring

--- Microphone_Array/FE_CPLD_Microphone_Encoder_Decoder.vhd
+++ Microphone_Array/FE_CPLD_Microphone_Encoder_Decoder.vhd
@@ -1,10 +1,12 @@
 ----------------------------------------------------------------------------
 --! @file FE_CPLD_Microphone_Encoder_Decoder.vhd
 --! @brief CPLD microphone array encoder/decoder component
---! @details  
+--! @details This component translates data from a variable array of microphones as well as a temperature, humidity, 
+--           pressure sensor and transmits that data to an decoder on another system.  The component also translates above
+--           serial stream of data into commands to configure the microphone array.
 --! @author Tyler Davis
---! @date 2019
---! @copyright Copyright 2019 Flat Earth Inc
+--! @date 2020
+--! @copyright Copyright 2020 Flat Earth Inc
 --
 --  Permission is hereby granted, free of charge, to any person obtaining a copy
 --  of this software and associated documentation files (the "Software"), to deal
@@ -73,9 +75,7 @@ entity FE_CPLD_Microphone_Encoder_Decoder is
           
     serial_data_out       : out std_logic;
     serial_data_in        : in std_logic                      := '0';
-    serial_clk_in         : in  std_logic                     := '0';
-    
-    debug_out             : out std_logic_vector(7 downto 0) := (others => '0')
+    serial_clk_in         : in  std_logic                     := '0'
   );
     
 end entity FE_CPLD_Microphone_Encoder_Decoder;
@@ -117,11 +117,11 @@ signal DATA_HEADER  : std_logic_vector(header_width-1 downto 0) := x"43504C44";
 signal CMD_HEADER   : std_logic_vector(header_width-1 downto 0) := x"46504741";
 
 -- Shift state signals
+constant shift_width          : integer := 8;
 signal shift_data             : std_logic_vector(31 downto 0) := (others => '0');
-signal shift_data_in          : std_logic_vector(7 downto 0) := (others => '0');
-signal shift_data_out         : std_logic_vector(7 downto 0) := (others => '0');
+signal shift_data_in          : std_logic_vector(shift_width-1 downto 0) := (others => '0');
+signal shift_data_out         : std_logic_vector(shift_width-1 downto 0) := (others => '0');
 signal byte_counter           : integer range 0 to 4 := 0;
-signal byte_counter_follower  : integer range 0 to 4 := 0;
 signal n_bytes                : integer range 0 to 4 := 0;
 signal bit_counter            : integer range 0 to 7 := 0;
 signal mic_counter            : integer range 0 to 64 := 0;
@@ -131,7 +131,9 @@ signal shift_en_n             : std_logic := '0';
 signal load_data              : std_logic := '0';
 signal packet_counter         : unsigned(31 downto 0) := (others => '0');
 signal sdo_mics               : integer range 0 to 64 := 16;
-constant shift_width          : integer := 8;
+
+-- TODO change the "trigger" to start shifting
+signal channel_trigger        : std_logic_vector(ch_width-1 downto 0) := (others => '0');
 
 
 -- Deserialization signals
@@ -140,11 +142,6 @@ signal parallel_data_r  : std_logic_vector(MAX_SDI_SIZE-1 downto 0) := (others =
 signal header_found     : std_logic := '0';
 signal read_bits        : integer range 0 to MAX_SDI_SIZE := 0;
 signal read_word_bits   : integer range 0 to MAX_SDI_SIZE := 0;
-signal sdo_mics_r       : integer range 0 to 32 := 16;
-signal cfg_data_r    : std_logic_vector(8*cfg_byte_width-1 downto 0) := (others => '0');
-signal cfg_out_valid_r : std_logic := '0';
-signal rgb_data_r       : std_logic_vector(8*rbg_byte_width-1 downto 0) := (others => '0');
-signal rgb_out_valid_r : std_logic := '0';
 
 
 signal send_valid : std_logic := '0';
@@ -159,6 +156,11 @@ signal shift_busy     : std_logic := '0';
 type mic_array_data is array (n_mics-1 downto 0) of std_logic_vector(mic_data_width-1 downto 0);
 signal mic_input_data_r : mic_array_data := (others => (others => '0'));
 signal bme_input_data_r : std_logic_vector(bme_data_width-1 downto 0) := (others => '0');
+signal sdo_mics_r       : integer range 0 to 32 := 16;
+signal cfg_data_r    : std_logic_vector(8*cfg_byte_width-1 downto 0) := (others => '0');
+signal cfg_out_valid_r : std_logic := '0';
+signal rgb_data_r       : std_logic_vector(8*rbg_byte_width-1 downto 0) := (others => '0');
+signal rgb_out_valid_r : std_logic := '0';
 
 
 -- Create states for the output state machine
@@ -189,6 +191,7 @@ signal sdi_valid_state : valid_state := idle;
 
 begin 
 
+-- Create a serializer for the data using the shift width
 serial_shift_map: Generic_Shift_Container    
 generic map (
   data_width => shift_width
@@ -226,7 +229,7 @@ begin
   elsif rising_edge(serial_clk_in) then 
   
     -- When the first data packet is received, start shifting the DATA_HEADER out
-    if mic_input_channel(ch_width-1 downto 0) = "0000" then --n_drivers then -- Note: SignalTap seems to mess with the comparison...
+    if mic_input_channel(ch_width-1 downto 0) = channel_trigger then -- TODO: Create new start condition.
       start_shifting <= '1';
       packet_counter <= packet_counter + 1;
     else
@@ -249,56 +252,80 @@ end process;
 data_out_transition_process : process(serial_clk_in,reset_n)
 begin
   if reset_n = '0' then 
-
+    cur_sdo_state   <= idle;
+    next_sdo_state  <= idle;
   elsif rising_edge(serial_clk_in) then 
     case cur_sdo_state is 
       when idle => 
+      
+        -- Wait for the start_shifting signal to load the header
         if start_shifting = '1' then 
           cur_sdo_state <= load_header;
+          
+        -- Otherwise, stay idle
         else  
           cur_sdo_state <= idle;
         end if;
         
       when load_header =>
+        -- Transition to the shift states, then set the next state to load 
         cur_sdo_state <= load_shift_reg;
         next_sdo_state <= load_packet_number;
         
       when load_packet_number =>
+        -- Transition to the shift states, then set the next state
         cur_sdo_state <= load_shift_reg;
         next_sdo_state <= load_n_mics;
         
       when load_n_mics =>
+        -- Transition to the shift states, then set the next state
         cur_sdo_state <= load_shift_reg;
         next_sdo_state <= load_temp;
         
       when load_temp =>
+        -- Transition to the shift states, then set the next state
         cur_sdo_state <= load_shift_reg;
         next_sdo_state <= load_pressure;
         
       when load_pressure =>
+        -- Transition to the shift states, then set the next state
         cur_sdo_state <= load_shift_reg;
         next_sdo_state <= load_humidity;
         
       when load_humidity =>
+        -- Transition to the shift states, then set the next state
         cur_sdo_state <= load_shift_reg;
         next_sdo_state <= load_mics;
         
       when load_mics =>
+        -- Transition to the shift states, decide whether another set of mic data needs to be loaded
         cur_sdo_state <= load_shift_reg;
+        
+        -- If the last mic data hasn't been loaded, keep loading the mics
         if mic_counter - 1 < sdo_mics then 
           next_sdo_state <= load_mics;
+        
+        -- Otherwise, go idle after the transfer
         else
           next_sdo_state <= idle;
         end if;
       
+      
       when load_shift_reg =>
+        -- Immediately transition to the wait state
         cur_sdo_state <= shift_wait;
       
       when shift_wait =>
+        -- If the specified number of bytes have been sent, transition to the next load state before the shift register 
+        -- "empties"
         if byte_counter = n_bytes and bit_counter = shift_width - 3 then 
           cur_sdo_state <= next_sdo_state;
+          
+        -- If there are still more bytes to load, load the next byte into the register
         elsif byte_counter < n_bytes and bit_counter = shift_width - 2 then 
           cur_sdo_state <= load_shift_reg;
+          
+        -- Otherwise, stay in the wait state
         else
           cur_sdo_state <= shift_wait;
         end if;
@@ -316,57 +343,87 @@ begin
   elsif rising_edge(serial_clk_in) then 
     case cur_sdo_state is 
       when idle => 
+        -- Reset the counters and signal the component is no longer busy
         bit_counter   <= 0;
         mic_counter <= 0;
         busy <= '0';
         
       when load_header =>
+        -- Load the header into the shift register
         shift_data <= DATA_HEADER;
+        
+        -- Set the number of bytes to transfer and reset the byte counter
         n_bytes <= header_byte_width;
         byte_counter <= 0;
         
       when load_packet_number =>
+        -- Load the packet counter into the shift register
         shift_data <= std_logic_vector(packet_counter);
+        
+        -- Set the number of bytes to transfer and reset the byte counter
         n_bytes <= packet_cntr_byte_width;
         byte_counter <= 0;
         
       when load_n_mics =>
+        -- Load the number of mics into the shift register
         shift_data(7 downto 0) <= std_logic_vector(to_unsigned(sdo_mics,8));
+        -- Set the number of bytes to transfer and reset the byte counter
         n_bytes <= n_mic_byte_width;
         byte_counter <= 0;
         
       when load_temp =>
+        -- Load the temperature into the shift register
         shift_data(8*temp_byte_width-1 downto 0) <= bme_input_data_r(8*temp_byte_location-1 downto 8*(temp_byte_location-temp_byte_width));
+        
+        -- Set the number of bytes to transfer and reset the byte counter
         n_bytes <= temp_byte_width;
         byte_counter <= 0;
         
       when load_pressure =>
+        -- Load the pressure into the shift register
         shift_data(8*pressure_byte_width-1 downto 0) <= bme_input_data_r(8*pressure_byte_location-1 downto 8*(pressure_byte_location-pressure_byte_width));
+        
+        -- Set the number of bytes to transfer and reset the byte counter
         n_bytes <= pressure_byte_width;
         byte_counter <= 0;
         
       when load_humidity =>
+        -- Load the humidity into the shift register
         shift_data(8*humid_byte_width-1 downto 0) <= bme_input_data_r(8*humid_byte_location-1 downto 8*(humid_byte_location-humid_byte_width));
+        
+        -- Set the number of bytes to transfer and reset the byte counter
         n_bytes <= humid_byte_width;
         byte_counter <= 0;
       
       when load_mics =>
+        -- Load the next microphone into the shift register
         mic_counter <= mic_counter + 1;
         shift_data(8*mic_byte_width-1 downto 0) <= mic_input_data_r(mic_counter_follower);
+        
+        -- Set the number of bytes to transfer and reset the byte counter
         n_bytes <= mic_byte_width;
         byte_counter <= 0;
       
       when load_shift_reg =>
+      
+        -- Update the microphone follower
         mic_counter_follower <= mic_counter;
+        
+        -- Reset the bit counter and increment the byte counter
         bit_counter <= 0;
         byte_counter <= byte_counter + 1;
+        
+        -- Load the next set of data into the shift component
         shift_data_in <= shift_data(8*(n_bytes-byte_counter)-1 downto 8*(n_bytes-byte_counter-1));
         load_data <= '1';
+        
+        -- Signal the component is busy
         busy <= '1';
       
       when shift_wait =>
+      
+        -- Increment the bit counter and reset the shift componet load signal
         bit_counter <= bit_counter + 1;
-        byte_counter_follower <= byte_counter;
         load_data <= '0';
       
       when others => 
@@ -381,7 +438,8 @@ begin
   if reset_n = '0' then 
     parallel_data_r <= (others => '0');
   elsif rising_edge(serial_clk_in) then 
-      parallel_data_r <= parallel_data_r(MAX_SDI_SIZE-2 downto 0) & serial_data_in;
+    -- Always shift the new serial bit into the end of the register
+    parallel_data_r <= parallel_data_r(MAX_SDI_SIZE-2 downto 0) & serial_data_in;
   end if;
 end process;
 
@@ -390,10 +448,16 @@ begin
   if reset_n = '0' then 
     read_bits <= 0;
   elsif rising_edge(serial_clk_in) then 
+  
+    -- If the input state machine is idle, don't count the bits coming into the component
     if cur_sdi_state = idle then 
       read_bits <= 0;
+      
+    -- When the counter reaches the current number of expected bits, reset it
     elsif read_bits = read_word_bits - 1 then 
       read_bits <= 0;
+      
+    -- Otherwise, increment the bit counter
     else
       read_bits <= read_bits + 1;
     end if;
@@ -407,12 +471,19 @@ begin
   elsif rising_edge(serial_clk_in) then 
     case cur_sdi_state is
       when idle => 
+      
+        -- If the header has been read, transition to reading the number of mics
         if parallel_data_r(8*header_byte_width-1 downto 0) = CMD_HEADER then
           cur_sdi_state <= read_mics;
+          
+        -- Otherwise, remain idle
         else
           cur_sdi_state <= idle;
         end if;
+        
       when read_mics =>
+      
+        -- Once the number of microphones have been read, read the mic configuration
         if read_bits = read_word_bits - 2 then 
           cur_sdi_state <= read_enable;
         else
@@ -420,6 +491,8 @@ begin
         end if;
       
       when read_enable =>
+        
+        -- Once the mic configuration has been read, read the rgb configuration
         if read_bits = read_word_bits - 2 then 
           cur_sdi_state <= read_rgb;
         else
@@ -427,16 +500,18 @@ begin
         end if;
       
       when read_rgb =>
+        
+        -- Once the rbg LED configuration has been read, send the valid pulse
         if read_bits = read_word_bits - 2 then 
           cur_sdi_state <= valid_pulse;
         else
           cur_sdi_state <= read_rgb;
         end if;
       
+      -- Immediately go idle
       when valid_pulse =>
         cur_sdi_state <= idle;
         
-      
       when others =>
     end case;
   end if;
@@ -452,14 +527,20 @@ begin
         send_valid <= '0';
 
       when read_mics =>
+        
+        -- "Shift in" the number of microphones
         sdo_mics_r <= to_integer(unsigned(parallel_data_r(8*n_mic_byte_width-1 downto 0)));
         read_word_bits <= 8*n_mic_byte_width;
 
       when read_enable =>
+        
+        -- "Shift in" the mic configuration
         cfg_data_r <= parallel_data_r(8*cfg_byte_width-1 downto 0);
         read_word_bits <= 8*cfg_byte_width;
       
       when read_rgb =>
+      
+        -- "Shift in" the rbg LED configuration 
         rgb_data_r <= parallel_data_r(8*rbg_byte_width-1 downto 0);
         read_word_bits <= 8*rbg_byte_width;
 
@@ -479,18 +560,28 @@ begin
   elsif rising_edge(sys_clk) then 
     case sdi_valid_state is 
       when idle =>
+      
+        -- Once the data has been read in, send a valid pulse at the system clock frequency (Avalon streaming)
         if send_valid = '1' then 
           sdi_valid_state <= pulse;
+          
+        -- Otherwise, stay idle
         else
           sdi_valid_state <= idle;
         end if;
       
       when pulse => 
+        
+        -- Transition to the wait state
         sdi_valid_state <= low_wait;
         
       when low_wait =>
+      
+        -- When "send" signal goes low (slower clock frequency), go idle again
         if send_valid = '0' then 
           sdi_valid_state <= idle;
+          
+        -- Otherwise, wait for the valid to go low
         else
           sdi_valid_state <= low_wait;
         end if;
@@ -498,7 +589,6 @@ begin
       when others =>    
     end case;
   end if;
-  
 end process; 
 
 sdi_data_valid_process : process(sys_clk,reset_n)
@@ -510,7 +600,11 @@ begin
     case sdi_valid_state is 
       when idle =>
       
+        -- Do nothing
+        
       when pulse => 
+        
+        -- Pulse the valid Avalon streaming signals
         rgb_out_valid_r <= '1';
         cfg_out_valid_r <= '1';
         
@@ -528,17 +622,13 @@ serial_data_out <= shift_data_out(shift_width-1);
 -- Map the busy signal 
 busy_out <= busy;
 
+-- Map the valid signals
 rgb_out_valid <= rgb_out_valid_r; 
 cfg_out_valid <= cfg_out_valid_r;
 
+-- Map the data signals
 cfg_out_data <= cfg_data_r;
 rgb_out_data <= rgb_data_r;
-
-debug_out(0) <= load_data;
-debug_out(1) <= rgb_out_valid_r;
-debug_out(2) <= cfg_out_valid_r;
-debug_out(3) <= start_shifting;
-debug_out(4) <= send_valid;
 
 end architecture rtl;
 
