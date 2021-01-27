6e77586de2596575b1d85ec116c88c4d91a9608b
Dylan Wickham <wickham@flatearthinc.com>
2021-01-27 15:44:48 -0700

    Added On Chip Ram userspace and kernel driver

---
+++ on_chip_RAM/userspace_driver/protocol_lws_minimal.c
@@ -0,0 +1,215 @@
+/*
+ * ws protocol handler plugin for "lws-minimal"
+ *
+ * Written in 2010-2019 by Andy Green <andy@warmcat.com>
+ *
+ * This file is made available under the Creative Commons CC0 1.0
+ * Universal Public Domain Dedication.
+ *
+ * This version holds a single message at a time, which may be lost if a new
+ * message comes.  See the minimal-ws-server-ring sample for the same thing
+ * but using an lws_ring ringbuffer to hold up to 8 messages at a time.
+ */
+
+#if !defined (LWS_PLUGIN_STATIC)
+#define LWS_DLL
+#define LWS_INTERNAL
+#include <libwebsockets.h>
+#endif
+
+#include <string.h>
+
+/* one of these created for each message */
+
+struct msg {
+	void *payload; /* is malloc'd */
+	size_t len;
+};
+
+/* one of these is created for each client connecting to us */
+
+struct per_session_data__minimal {
+	struct per_session_data__minimal *pss_list;
+	struct lws *wsi;
+	int last; /* the last message number we sent */
+};
+
+/* one of these is created for each vhost our protocol is used with */
+
+struct per_vhost_data__minimal {
+	struct lws_context *context;
+	struct lws_vhost *vhost;
+	const struct lws_protocols *protocol;
+
+	struct per_session_data__minimal *pss_list; /* linked-list of live pss*/
+
+	struct msg amsg; /* the one pending message... */
+	int current; /* the current message number we are caching */
+};
+
+typedef unsigned char* message_type;
+void (*receive_pointer_function)(message_type,uint length);
+void attach_message_receive_callback(const void* handler_callback)
+{	
+	receive_pointer_function = handler_callback;
+}
+
+/* destroys the message when everyone has had a copy of it */
+
+static void
+__minimal_destroy_message(void *_msg)
+{
+	struct msg *msg = _msg;
+
+	free(msg->payload);
+	msg->payload = NULL;
+	msg->len = 0;
+}
+
+struct per_vhost_data__minimal *g_vhd;
+
+extern int active;
+
+static int
+callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
+			void *user, void *in, size_t len)
+{
+	struct per_session_data__minimal *pss =
+			(struct per_session_data__minimal *)user;
+	struct per_vhost_data__minimal *vhd =
+			(struct per_vhost_data__minimal *)
+			lws_protocol_vh_priv_get(lws_get_vhost(wsi),
+					lws_get_protocol(wsi));
+	g_vhd = vhd;
+	int m;
+
+	switch (reason) {
+	case LWS_CALLBACK_PROTOCOL_INIT:
+		printf("Hello1\n");
+		vhd = lws_protocol_vh_priv_zalloc(lws_get_vhost(wsi),
+				lws_get_protocol(wsi),
+				sizeof(struct per_vhost_data__minimal));
+		vhd->context = lws_get_context(wsi);
+		vhd->protocol = lws_get_protocol(wsi);
+		vhd->vhost = lws_get_vhost(wsi);
+		break;
+
+	case LWS_CALLBACK_ESTABLISHED:
+		printf("Hello2\n");
+		/* add ourselves to the list of live pss held in the vhd */
+		lws_ll_fwd_insert(pss, pss_list, vhd->pss_list);
+		pss->wsi = wsi;
+		pss->last = vhd->current;
+		active = 1;
+		printf("Connection made\n");
+		break;
+
+	case LWS_CALLBACK_CLOSED:
+		printf("Hello3\n");
+		/* remove our closing pss from the list of live pss */
+		lws_ll_fwd_remove(struct per_session_data__minimal, pss_list,
+				  pss, vhd->pss_list);
+		break;
+
+	case LWS_CALLBACK_SERVER_WRITEABLE:
+		printf("Hello4\n");
+		if (!vhd->amsg.payload)
+			break;
+
+		if (pss->last == vhd->current)
+			break;
+
+		/* notice we allowed for LWS_PRE in the payload already */
+		m = lws_write(wsi, ((unsigned char *)vhd->amsg.payload) +
+			      LWS_PRE, vhd->amsg.len, LWS_WRITE_TEXT);
+		if (m < (int)vhd->amsg.len) {
+			lwsl_err("ERROR %d writing to ws\n", m);
+			return -1;
+		}
+
+		pss->last = vhd->current;
+		break;
+
+	case LWS_CALLBACK_RECEIVE:
+		printf("Hello5\n");
+		(receive_pointer_function)((message_type)in,len);
+		break;
+
+	default:
+		printf("Hello6\n");
+		break;
+	}
+
+	return 0;
+}
+
+void 
+send_message_all_clients(char* message, uint length)
+{			
+	if (g_vhd->amsg.payload)
+		__minimal_destroy_message(&g_vhd->amsg);
+
+	g_vhd->amsg.len = length;
+	/* notice we over-allocate by LWS_PRE */
+	g_vhd->amsg.payload = malloc(LWS_PRE + length);
+	
+	if (!g_vhd->amsg.payload) {
+		lwsl_user("OOM: dropping\n");
+		return;
+	}
+	
+	memcpy((char *)g_vhd->amsg.payload + LWS_PRE, message, length);
+	g_vhd->current++;
+	
+	/*
+		* let everybody know we want to write something on them
+		* as soon as they are ready
+		*/
+	lws_start_foreach_llp(struct per_session_data__minimal **,
+					ppss, g_vhd->pss_list) {
+		lws_callback_on_writable((*ppss)->wsi);
+	} lws_end_foreach_llp(ppss, pss_list);
+	
+}
+
+
+#define LWS_PLUGIN_PROTOCOL_MINIMAL \
+	{ \
+		"lws-minimal", \
+		callback_minimal, \
+		sizeof(struct per_session_data__minimal), \
+		128, \
+		0, NULL, 0 \
+	}
+#if !defined (LWS_PLUGIN_STATIC)
+
+/* boilerplate needed if we are built as a dynamic plugin */
+
+static const struct lws_protocols protocols[] = {
+	LWS_PLUGIN_PROTOCOL_MINIMAL
+};
+
+int
+init_protocol_minimal(struct lws_context *context,
+		      struct lws_plugin_capability *c)
+{
+	if (c->api_magic != LWS_PLUGIN_API_MAGIC) {
+		lwsl_err("Plugin API %d, library API %d", LWS_PLUGIN_API_MAGIC,
+			 c->api_magic);
+		return 1;
+	}
+
+	c->protocols = protocols;
+	c->count_protocols = LWS_ARRAY_SIZE(protocols);
+	c->extensions = NULL;
+	c->count_extensions = 0;
+
+	return 0;
+}
+
+int
+destroy_protocol_minimal(struct lws_context *context)
+{
+	return 0;
+}
+#endif
